6cd63b3fb36dbf16f854cdc9e936f0bdf9cc1dd6
Cwhist <huichun97@naver.com>
2020-12-04 05:10:49 +0900

    Update validation in Course class

--- src/main/java/kr/ac/hansung/model/Course.java
+++ src/main/java/kr/ac/hansung/model/Course.java
@@ -1,5 +1,7 @@
 package kr.ac.hansung.model;
 
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
@@ -22,17 +24,20 @@ public class Course {
 	@NotEmpty(message="The semester infomation cannot be empty")
 	private String semester;
 	
-	@Size(min=2, max=100, message="Name must be between 2 and 30 chars")
+	@Size(min=2, max=30, message="Name must be between 2 and 30 chars")
 	@NotEmpty(message="The course name cannot be empty")
 	private String name;
 	
+	@Size(min=2, max=10, message="Name must be between 2 and 10 chars")
 	@NotEmpty(message="The type infomation cannot be empty")
 	private String type;
 	
+	@Size(min=2, max=10, message="Name must be between 2 and 10 chars")
 	@NotEmpty(message="The prof infomation cannot be empty")
 	private String prof;
 	
-	@NotEmpty(message="The credit infomation cannot be empty")
+	@DecimalMin(value="2", message="Credit must be higher than 2")
+	@DecimalMax(value="12", message="Credit must be lower than 12")
 	private int credit;
 
 }
