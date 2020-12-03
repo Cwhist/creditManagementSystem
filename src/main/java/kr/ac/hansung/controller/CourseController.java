b0199420634b5468acb4000b6f75a6fe7b3454a0
Cwhist <huichun97@naver.com>
2020-12-04 05:12:10 +0900

    Add register course function

--- src/main/java/kr/ac/hansung/controller/CourseController.java
+++ src/main/java/kr/ac/hansung/controller/CourseController.java
@@ -2,12 +2,17 @@ package kr.ac.hansung.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.ac.hansung.model.Course;
+import kr.ac.hansung.model.Offer;
 import kr.ac.hansung.service.CourseService;
 
 @Controller
@@ -16,6 +21,7 @@ public class CourseController {
 	@Autowired
 	private CourseService courseService;
 	
+	// 학년/학기별 이수 총 학점
 	@RequestMapping("/courses")
 	public String showCourses(Model model) {
 		
@@ -28,6 +34,7 @@ public class CourseController {
 		return "courses";
 	}
 	
+	// 학기별 수강 내역
 	@RequestMapping("/detailview")
 	public String viewDetail(Model model) {
 		
@@ -35,6 +42,38 @@ public class CourseController {
 		model.addAttribute("courses", courses);
 		
 		return "detailview";
+	}
+	
+	
+	// 수강신청
+	@RequestMapping("/registercourse")
+	public String registerCourse(Model model) {
+		
+		model.addAttribute("course", new Course());
+		
+		return "registercourse";
+	}
+	
+	
+	// 수강 신청 처리
+	@RequestMapping("/doregister")
+	public String doRegister(Model model, @Valid Course course, BindingResult result) {
+		
+		if(result.hasErrors()) {
+			System.out.println("== Form data does not validated ==");
+			
+			List<ObjectError> errors = result.getAllErrors();
+			
+			for(ObjectError error : errors) {
+				System.out.println(error.getDefaultMessage());
+			}
+			
+			return "registercourse";
+		}
+		
+		courseService.insert(course);
+		
+		return "courseregistered";
 	}
 	
 
---
+++ src/main/webapp/WEB-INF/views/registercourse.jsp
@@ -0,0 +1,56 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="sf" uri="http://www.springframework.org/tags/form" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>수강 신청</title>
+
+<link rel="stylesheet" type="text/css" 
+	href="${pageContext.request.contextPath}/resources/css/main.css" >
+</head>
+<body>
+	<sf:form method="post" action="${pageContext.request.contextPath}/doregister" modelAttribute="course">
+	<table class="styled-table">
+		<thead>
+			<tr>
+			<th> 구분 </th>
+			<th> 입력 </th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+			<th> 교과목명 </th>
+			<th> <sf:input class="control" type="text" path="name"/> <br></br>
+			<sf:errors class="error" path="name"/> </th>
+			</tr>
+			
+			<tr>
+			<th> 교과구분 </th>
+			<th> <sf:input class="control" type="text" path="type"/> <br></br>
+			<sf:errors class="error" path="type"/> </th>
+			</tr>
+			
+			<tr>
+			<th> 담당교수 </th>
+			<th> <sf:input class="control" type="text" path="prof"/> <br></br>
+			<sf:errors class="error" path="prof"/> </th>
+			</tr>
+			
+			<tr>
+			<th> 학점 </th>
+			<th> <sf:input class="control" type="text" path="credit"/> <br></br>
+			<sf:errors class="error" path="credit"/> </th>
+			</tr>
+			
+			<tr> <th colspan="2" align="center"> <input type="submit" value="등록"/> </th></tr>
+		</tbody>
+	</table>
+	</sf:form>
+	
+	<a href="<c:url value="/"/>"> back </a>
+
+</body>
+</html>
